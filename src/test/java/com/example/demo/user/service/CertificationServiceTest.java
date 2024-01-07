bea4648a21f325c0edc864b5d03e45710730d39c
akfrdma0125 <nogain1215@gmail.com>
2024-01-07 15:21:25 +0900

    feat: CertificationService 메서드에 테스트 코드 추가

---
+++ src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CertificationServiceTest {
+
+    @Test
+    void 이메일과_컨텐츠가_제대로_만들어져서_보내진다() {
+        //given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        //when
+        certificationService.send("jeohyoo1229@gmail.com","1234",1L);
+
+        //then
+        assertThat(fakeMailSender.email).isEqualTo("jeohyoo1229@gmail.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo("Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=1234");
+    }
+}
